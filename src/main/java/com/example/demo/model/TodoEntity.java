4cc0f7ebb71175e8ed439000111a25c5f609b72a
Sung Chae Won <scw226@sookmyung.ac.kr>
2023-06-19 05:44:19 +0900

    백엔드 개발 18 @Query 사용하기

--- src/main/java/com/example/demo/model/TodoEntity.java
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -3,7 +3,6 @@ package com.example.demo.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import org.hibernate.annotations.GenericGenerator;
 import lombok.AllArgsConstructor;
@@ -11,14 +10,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
 @Table(name ="Todo")
-@NamedQuery(name="TodoRepository.searchByUserId",
-	query ="select t from TodoEntity t where t.userId = ?1")
 public class TodoEntity {
 	
 	@Id
